cea6280cfe01b3b21e9cc2f6c762f0bbbf7b6f0f
lenguyet-ha <lenguyetha14@gmail.com>
2025-09-09 18:56:53 +0700

    init shared folder

---
+++ src/shared/services/hashing.service.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Injectable } from '@nestjs/common';
+import { compare, hash } from 'bcrypt';
+const saltRounds = 10;
+
+@Injectable()
+export class HashingService {
+    hash(value: string) {
+        return hash(value, saltRounds);
+    }
+
+    compare(value: string, hash: string) {
+        return compare(value, hash);
+    }
+}
