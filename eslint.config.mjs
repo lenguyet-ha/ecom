45a5dc8cedf4315eae978e39f788b3968b2c2acf
lenguyet-ha <lenguyetha14@gmail.com>
2025-09-10 09:56:46 +0700

    update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,6 +35,7 @@ export default tseslint.config(
             '@typescript-eslint/no-unsafe-call': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             'prettier/prettier': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
         },
     },
 );
