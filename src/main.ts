258c4891df6d4a46daf5166e1163d6e6179d008d
lenguyet-ha <lenguyetha14@gmail.com>
2025-09-30 16:31:12 +0700

    update file main

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,17 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const reflector = app.get(Reflector);
-  
-  app.useGlobalPipes(new ZodValidationPipe());
-  // Tạm tắt để test
-  // app.useGlobalInterceptors(new ZodSerializerInterceptor(reflector));
-  app.useGlobalFilters(new HttpExceptionFilter());
-  
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const reflector = app.get(Reflector);
+    app.enableCors({
+        origin: 'http://localhost:3000', // Thay bằng origin của frontend (e.g., http://localhost:3000)
+        credentials: false, // Nếu dùng cookies/auth
+    });
+    app.useGlobalPipes(new ZodValidationPipe());
+    // Tạm tắt để test
+    // app.useGlobalInterceptors(new ZodSerializerInterceptor(reflector));
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    await app.listen(4000);
 }
 void bootstrap();
